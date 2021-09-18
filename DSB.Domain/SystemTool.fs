aabd9a1ecd7512045844b21cb4318ff01f76ee6a
Kevin Avignon <kevin.avignon5@gmail.com>
2021-09-18 18:01:40 -0400

    #2: Defined the fundamental domain of a system tool.
    Without getting in the specifics of how we handle a system tool, let's rather focus on how we want to represent a system tool. A system tool is going to be a development utility (ex: Git) that's going to be commonly used by a developer. Such an entity would need multiple characteristics to describe what it is such as:
    - The name of the tool (title),
    - Its current semantic version,
    - A high-level description if necessary,
    - The selected mechanism to download the tool from the web
    - Its absolute path on the machine,
    - Whether it should be treated as an environment variable.
    
    As we move further in the implementation, we'll be using the domain knowledge acquired here in the infrastructure and define the necessary code to download system tools from the web. Moreover, we'll also have to define an easy-going framework for the users to use if they want to add new default system tools to DBS.

---
+++ DSB.Domain/SystemTool.fs
@@ -0,0 +1,75 @@
+﻿namespace DSB.Domain
+
+open System
+
+module DefaultProgram =
+    
+    type AbsolutePath = AbsolutePath of string
+
+    type SemanticVersion = SemanticVersion of string
+
+    type DownloadLocation = DownloadLocation of Uri
+
+    type OperatingSystem =
+        | Linux
+        | Mac
+        | Windows
+
+    type SourceCodeInstaller =
+        | X64
+        | Arm64
+        | X86
+
+    type WindowsBinaryInstaller =
+        | Arm64
+        | X64
+        | X86
+
+    type MacBinaryInstaller =
+        | X64
+
+    type LinuxBinaryInstaller =
+        | Arm32
+        | Arm32Alpine
+        | Arm64
+        | Arm64Alpine
+        | X64
+        | X64Alpine
+
+    type BinaryInstaller =
+        | Linux of LinuxBinaryInstaller
+        | Mac of MacBinaryInstaller
+        | Windows of WindowsBinaryInstaller
+
+    type InstallationMode =
+        | Installer of SourceCodeInstaller
+        | Binaries of BinaryInstaller
+        | Script of string
+
+    type ToolSystemInformation = {
+        SystemPath: AbsolutePath option
+        CurrentVersion: SemanticVersion
+    }
+
+    type InstallerInformation = {
+        Url: DownloadLocation
+        OperatingSystem: OperatingSystem
+        SelectedInstallationMode: InstallationMode
+    }
+
+    type SystemTool = {
+        Title: string
+        ToolSummary: string option
+        DownloadLocation: DownloadLocation
+        SystemInformation: ToolSystemInformation
+        Installer: InstallerInformation
+        IsEnvironmentVariable: bool
+    }
+
+module DevelopmentMachine =
+    open DefaultProgram
+
+    type Environment = {
+        OS: OperatingSystem
+        DefaultTools: SystemTool list option
+    }
